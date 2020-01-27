0b6052224ccf7478266fb3cea6c56389af430dee
Qing <chingwang.qw@gmail.com>
2020-01-27 17:47:39 +0000

    installed sematic ui css

--- src/App.js
+++ src/App.js
@@ -1,25 +1,12 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1 className="app-name">What to Watch</h1>
+      <button className="ui inverted olive button">Click Me</button>
+    </>
   );
 }
 
