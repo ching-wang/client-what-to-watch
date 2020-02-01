1d4262515ec07157b4d3641c372bdb4685575ba8
Qing <chingwang.qw@gmail.com>
2020-02-01 22:02:20 +0000

    sort out query search result

--- src/pages/searchResults.js
+++ src/pages/searchResults.js
@@ -2,12 +2,15 @@ import React, { useState, useEffect } from "react";
 import API from "../adapters/API";
 import { Container, Card } from "semantic-ui-react";
 import { SearchResultCard } from "../components/SearchResultCard";
+import { useLocation } from "react-router-dom";
+import * as queryString from "query-string";
 
 export const SearchResults = props => {
   const [results, setResults] = useState([]);
+  const location = useLocation();
+  const query = queryString.parse(location.search).s;
   useEffect(() => {
-    // console.log(props.location.search);
-    API.searchMovies("action").then(res => setResults(res.Search));
+    API.searchMovies(query).then(res => setResults(res.Search));
   }, []);
   if (!results) {
     return <></>;
@@ -18,7 +21,7 @@ export const SearchResults = props => {
         <h1>Search results</h1>
         <Card.Group>
           {results.map(r => (
-            <SearchResultCard searchResult={r} />
+            <SearchResultCard key={r.imdbID} searchResult={r} />
           ))}
         </Card.Group>
       </div>
