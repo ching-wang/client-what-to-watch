64212cfd7fc289bb7a1a2cb9d6393e00d7c4901a
Qing <chingwang.qw@gmail.com>
2020-02-09 17:07:39 +0000

    adjusted pagination position

--- src/pages/searchResults.js
+++ src/pages/searchResults.js
@@ -30,15 +30,18 @@ export const SearchResults = ({ handleShowMovieCard }) => {
             />
           ))}
         </Card.Group>
-        <Pagination
-          boundaryRange={0}
-          defaultActivePage={1}
-          ellipsisItem={null}
-          firstItem={null}
-          lastItem={null}
-          siblingRange={1}
-          totalPages={10}
-        />
+        <br></br>
+        <Container textAlign="center">
+          <Pagination
+            boundaryRange={0}
+            defaultActivePage={1}
+            ellipsisItem={null}
+            firstItem={null}
+            lastItem={null}
+            siblingRange={1}
+            totalPages={10}
+          />
+        </Container>
       </div>
     </Container>
   );
