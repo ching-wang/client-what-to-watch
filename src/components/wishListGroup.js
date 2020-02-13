9208af2c45455eea925dde3556cd7683446e4863
Qing <chingwang.qw@gmail.com>
2020-02-13 14:00:29 +0000

    change color of the new-wishlist button

--- src/components/wishListGroup.js
+++ src/components/wishListGroup.js
@@ -39,15 +39,15 @@ const WishListGroup = ({ user }) => {
         </>
       ) : (
         <>
-          <Grid columns={3}>
-            <Grid.Column>
+        <br/>
+          <Grid>
               <h1>
                 You have {wishLists.length} &nbsp;
                 {wishLists.length === 1 ? "wishlist" : "wishlists"}
-                <NewWishlist />
               </h1>
-            </Grid.Column>
+              <NewWishlist />
           </Grid>{" "}
+          <br/>
           <Card.Group centered={true} itemsPerRow={4}>
             {wishLists.map(wishList => (
               <WishListCard
@@ -60,38 +60,6 @@ const WishListGroup = ({ user }) => {
           </Card.Group>
         </>
       )}
-
-      {/* <Grid columns={3}>
-        <Grid.Column>
-          <h1>
-            You have {wishLists.length} &nbsp;
-            {wishLists.length > 1 ? "wishLists" : "wishlist"}
-          </h1>
-        </Grid.Column>
-      </Grid>
-
-      <br />
-      <Card.Group centered={true} itemsPerRow={4}>
-        {wishLists.map(wishList => (
-          <WishListCard
-            key={wishList.id}
-            wishList={wishList}
-            use={user}
-            handleDeleteWishlist={deleteWishlist}
-          />
-        ))}
-      </Card.Group>
-
-      {wishLists.length < 1 ? (
-        <>
-          <Container textalign="center">
-            <h1 className="wishlist-hero">Why not create one now? </h1>
-            <NewWishlist />
-          </Container>{" "}
-        </>
-      ) : (
-        ""
-      )} */}
     </>
   );
 };
