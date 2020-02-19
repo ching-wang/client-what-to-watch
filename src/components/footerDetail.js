dc14aebd2a8ff4953fbf3ed6add88c0aef8be951
Qing <chingwang.qw@gmail.com>
2020-02-19 22:07:02 +0000

    add footer

--- src/components/footerDetail.js
+++ src/components/footerDetail.js
@@ -5,27 +5,16 @@ export const FooterDetail = () => {
   return (
     <>
       {" "}
-      <Container className="footer-container">
-        {/* <Grid divided inverted stackable>
-          <Grid.Row> */}
-        {/* <Grid.Column width={8}> */}
-        {/* <Header inverted as="h4" content="Find Me" /> */}
-        <Icon name="twitter" size="big" />
+      <Container className="footer-container" textAlign="center">
+        <Icon name="github" size="big" />
         &nbsp;
         <Icon name="facebook" size="big" />
         &nbsp;
         <Icon name="youtube" size="big" />
         &nbsp;
         <Icon name="instagram" size="big" />
-        {/* </Grid.Column> */}
-        {/* <Grid.Column width={8}>
-              <Header as="h4" inverted>
-                About
-              </Header>
-              <p>Add a film to your wishlist</p>
-            </Grid.Column> */}
-        {/* </Grid.Row>
-        </Grid> */}
+        <br />
+        <span> Created by Qing Wang</span>
       </Container>
     </>
   );
